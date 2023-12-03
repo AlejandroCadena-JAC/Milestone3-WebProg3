48c3e067552d13f257b8934dcaaec92084201724
Alejandro <2168579@johnabbottcollege.net>
2023-12-02 20:44:48 -0500

    Added source code

---
+++ ECommerceFinal/Ecommerce.Api.Customers/Interfaces/ICustomersProvider.cs
@@ -0,0 +1,12 @@
+﻿using Ecommerce.Api.Customers.Db;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Api.Customers.Interfaces
+{
+    public interface ICustomersProvider
+    {
+        Task<(bool IsSuccess, IEnumerable<Customer> Customers, string ErrorMessage)> GetCustomersAsync();
+        Task<(bool IsSuccess, Customer Customer, string ErrorMessage)> GetCustomerAsync(int id);
+    }
+}
---
+++ ECommerceFinal/Ecommerce.Api.Products/Interfaces/IProductsProvider.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Api.Products.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Api.Products.Interfaces
+{
+    public interface IProductsProvider
+    {
+
+        Task<(bool IsSuccess, IEnumerable<Product> Products, string ErrorMessage)> GetProductsAsync();
+
+        Task<(bool IsSuccess, Product Product, string ErrorMessage)> GetProductAsync(int id);
+
+    }
+}
